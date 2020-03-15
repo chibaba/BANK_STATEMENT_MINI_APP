0c500b23f330e6daa5a09193896fc66249c30ed9
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-03-15 05:55:41 +0100

    added bank statement for january

--- src/com/company/BankStatement.java
+++ src/com/company/BankStatement.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -12,14 +15,21 @@ public class BankStatement {
 
     public static void main(final String... args) throws IOException {
         final Path path = Paths.get(RESOURCES + args[0]);
-        final List<String>lines = Files.readAllLines(path);
+        final List<String> lines = Files.readAllLines(path);
         double total = 0d;
-        for(final String line: lines) {
-            final String[] columns =line.split(",");
-            final double amount = Double.parseDouble(columns[1]);
-            total += amount;
+        // adding january bank statement
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yy");
+        for (final String line : lines) {
+            final String[] columns = line.split(",");
+            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
         }
-        System.out.println("the total for all transaction is " + total);
+        System.out.println("the total for all transaction in January is " + total);
     }
 }
+
+
 //loading a csv file passed as a command line argument to the application
