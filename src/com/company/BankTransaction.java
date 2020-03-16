76109273015e4ae7b90964846e27080a4930e7be
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-03-16 02:51:38 +0100

    creating class of Bank statement

---
+++ src/com/company/BankTransaction.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class BankTransaction {
+private final LocalDate date;
+private final double amount;
+private final String description;
+
+ public BankTransaction(final LocalDate date, final double amount, final String description) {
+     this.date = date;
+     this
+ }
+}
