a592d2c7b8e4fcf0cac5a490911c6592c40318de
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-04-04 00:54:04 +0100

    final implementation of the bank transactionprcessor class cont

--- src/com/company/BankTransactionProcessor.java
+++ src/com/company/BankTransactionProcessor.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -27,5 +28,24 @@ public interface BankTransactionProcessor {
             result = bankTransactionSummarizer.summarize(result, bankTransaction);
         }
         return result;
+    }
+    public double calculateTotalInMonth(final Month month) {
+        return summarizeTransactions((acc, bankTransaction) ->
+                bankTransaction.getDate().getMonth() == month ? acc + bankTransaction.getAmount() : acc);
+    }
+    // ...
+
+    public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
+        final List<BankTransaction> result  = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransactionFilter.test(bankTransaction)) {
+                result.add(bankTransaction);
+            }
+
+        }
+        return bankTransactions;
+    }
+    Public List<BankTransaction> findTransactionGreaterThanEqual(final int amount) {
+        return findTransactions(bankTransaction -> bankTransaction.getAmount() >= amount);
     }
 }
